e8765a4ba460e55880ff1d3a9c1efe184c6bdf61
Romans <Romans.alaibi@hotmail.com>
2018-12-10 11:36:57 +0100

    added room & customer.

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,4 +1,44 @@
 package com.company;
 
-public class Room {
+public class Room extends Customer {
+    private String name;
+    private String SSN;
+    private String add;
+    private String number;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+    public void setAdd(String add) {
+        this.add = add;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public String getAdd() {
+        return add;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String toString() {
+        return String.format("%nName: %s%nSSN: %s%nAdress: %s%nNumber: %s%n",name,SSN,add,number);
+    }
 }
